c5ac07f41673b3f5ad1c42d39342173654710344
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2021-04-14 00:15:05 -0400

    Add --privexp command line option
    If given, this will also read a raw private exponent, and instead output
    an RSA Private Key.

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 #include <limits.h>
 #include <getopt.h>
 #include <openssl/bn.h>
-#include <openssl/rsa.h>
+#include <openssl/err.h>
 #include <openssl/pem.h>
+#include <openssl/rsa.h>
 
 #define MAX_MOD_SIZE        (OPENSSL_RSA_MAX_MODULUS_BITS * CHAR_BIT)
 #define DEFAULT_EXPONENT    65537ul
@@ -63,13 +64,16 @@ static void usage(void)
         " %s [options] <modulus-file>\n"
         "\n"
         "Options:\n"
-        " -e, --exponent EXP  Exponent, defaults to %lu\n"
-        "\n",
+        " -e, --exponent EXP    Exponent, defaults to %lu\n"
+        " -p, --privexp  FILE   Private exponent bignum file\n"
+        "\n"
+        "If --privexp is given, output format is a private key.\n",
         appname, DEFAULT_EXPONENT);
 }
 
 static unsigned long exponent = DEFAULT_EXPONENT;
 static const char *modfile;
+static const char *privexp_file;
 
 static void parse_opts(int argc, char *argv[])
 {
@@ -78,10 +82,11 @@ static void parse_opts(int argc, char *argv[])
 
     static struct option long_options[] = {
         {"exponent",    required_argument,  0,  'e'},
+        {"privexp",     required_argument,  0,  'p'},
         {NULL,          0,                  0,  0}
     };
 
-    while ((opt = getopt_long(argc, argv, "e:",
+    while ((opt = getopt_long(argc, argv, "e:p:",
                     long_options, &long_index)) != -1) {
         switch (opt) {
             case 'e':
@@ -89,6 +94,10 @@ static void parse_opts(int argc, char *argv[])
                     err("Invalid exponent: \"%s\"\n", optarg);
                     exit(1);
                 }
+                break;
+
+            case 'p':
+                privexp_file = optarg;
                 break;
 
             default:
@@ -157,6 +166,7 @@ out:
 int main(int argc, char *argv[])
 {
     BIGNUM *mod = NULL;
+    BIGNUM *privexp = NULL;
 
     appname = basename(argv[0]);
     parse_opts(argc, argv);
@@ -165,8 +175,15 @@ int main(int argc, char *argv[])
     if (!mod) {
         return 1;
     }
-    print_bn("Modulus", mod);
-   
+    print_bn("Public modulus (n)", mod);
+
+    if (privexp_file) {
+        privexp = read_bignum_file(privexp_file);
+        if (!privexp) {
+            return 1;
+        }
+        print_bn("Private exponent (d)", privexp);
+    }
 
     /* Parse exponent */
     BIGNUM *exp = BN_new();
@@ -174,7 +191,7 @@ int main(int argc, char *argv[])
         err("BN_set_word() failed\n");
         return 1;
     }
-    print_bn("Exponent", exp);
+    print_bn("Public exponent", exp);
 
     /* Create RSA key */
     RSA *rsa = RSA_new();
@@ -182,12 +199,36 @@ int main(int argc, char *argv[])
         err("RSA_new() failed\n");
         return 1;
     }
-    RSA_set0_key(rsa, mod, exp, NULL);
+    RSA_set0_key(rsa, mod, exp, privexp);
 
-    /* Write PEM-encoded RSA public key to stdout */
-    if (!PEM_write_RSA_PUBKEY(stdout, rsa)) {
-        err("PEM_write_RSAPublicKey() failed\n");
-        return 1;
+    if (privexp) {
+#if 0
+        /* Check the private key */
+        /**
+         * XXX: This doesn't work because we dont' have p and q:
+         * http://openssl.6102.n7.nabble.com/RSA-check-key-failure-0x407b093-value-missing-td50723.html
+         * https://github.com/openssl/openssl/blob/OpenSSL_1_1_1d/crypto/rsa/rsa_chk.c#L26-L30
+         */
+        if (RSA_check_key(rsa) != 1) {
+            err("Invalid private RSA key: %s\n",
+                    ERR_error_string(ERR_get_error(), NULL));
+        }
+        err("RSA private key pair is valid\n");
+#endif
+
+
+        /* Write PEM-encoded RSA private key to stdout */
+        if (!PEM_write_RSAPrivateKey(stdout, rsa, NULL, NULL, 0, NULL, NULL)) {
+            err("PEM_write_RSAPrivateKey() failed\n");
+            return 1;
+        }
+    }
+    else {
+        /* Write PEM-encoded RSA public key to stdout */
+        if (!PEM_write_RSA_PUBKEY(stdout, rsa)) {
+            err("PEM_write_RSAPublicKey() failed\n");
+            return 1;
+        }
     }
 
     return 0;
