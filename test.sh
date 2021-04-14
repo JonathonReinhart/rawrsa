4bcb30edc9294241e80873efc0143cfe96d73a5f
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2021-04-14 00:57:21 -0400

    Add primitive test script

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+EXP=65537
+
+scons
+./generate_rsa_key.py -e $EXP
+
+# Reconstruct public key
+./rawrsa -e $EXP pubmod.bin > public_out.pem
+
+# Reconstruct private key
+./rawrsa -e $EXP --privexp privexp.bin pubmod.bin > private_out.pem
+
+# Compare
+md5sum *.pem
+
+diff public.pem public_out.pem
+echo "Reconstructed public key matches generated PEM!"
+
+diff private.pem private_out.pem
+echo "Reconstructed private key matches generated PEM!"
